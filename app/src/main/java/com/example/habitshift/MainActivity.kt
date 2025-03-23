d9faf6d2417dec7072a17b5297c761d48fc33c90
Ruhaan <ruhaansande@gmail.com>
2025-03-23 17:29:51 +0530

    Added borders to buttons on Preferences & Suggestions

--- app/src/main/java/com/example/habitshift/MainActivity.kt
+++ app/src/main/java/com/example/habitshift/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HabitShiftTheme{ SuggestionsPage()}
+            HabitShiftTheme{ PreferencesPage()}
 
         }
     }
